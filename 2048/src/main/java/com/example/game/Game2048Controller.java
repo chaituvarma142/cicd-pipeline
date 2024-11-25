7e780ab9c8a3e1a92a11a681dfbec49add855d19
chaituvarma142 <chaitanyavarma142@gmail.com>
2024-11-25 13:02:07 -0600

    initial code with frontend

---
+++ 2048/src/main/java/com/example/game/Game2048Controller.java
@@ -0,0 +1,30 @@
+package com.example.game;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/game")
+public class Game2048Controller {
+
+    private GameLogic game;
+
+    public Game2048Controller() {
+        this.game = new GameLogic();
+    }
+
+    @GetMapping("/board")
+    public int[][] getBoard() {
+        return game.getBoard();
+    }
+
+    @PostMapping("/move")
+    public int[][] makeMove(@RequestParam String direction) {
+        game.move(direction);
+        return game.getBoard();
+    }
+
+    @GetMapping("/gameover")
+    public boolean isGameOver() {
+        return game.isGameOver();
+    }
+}
